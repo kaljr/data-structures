69c3e505430e0988aaf9b52027cb25c3ac6abbbc
kaljr <kenny.langston@gmail.com>
2015-03-25 17:46:03 -0700

    Finished data structures

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -2,33 +2,36 @@
 
 
 var Queue = function() {
-
+  this.storage = {};
   this.length = 0;
 
 };
 
 
 Queue.prototype.size = function() {
-  if (this.length <= 0) {
-    this.length = 0;
-    return 0;
-  }
   return this.length;
 };
 
 Queue.prototype.enqueue = function(val) {
-  this[this.length-1] = val;
+  this.storage[this.length] = val;
   this.length ++;
 };
 
 Queue.prototype.dequeue = function(val) {
-  var result = this[0];
-  for(var i = 0; i<this.length-1; i++) {
-    this[i] = this[i+1];
+  var result = null;
+
+  if(this.length > 0) {
+    result = this.storage[0];
+    for (var i = 0; i<this.length-1; i++) {
+      this.storage[i] = this.storage[i+1];
+    }
+    delete this.storage[this.length-1];
+    this.length --;
+    return result;
   }
-  delete this[this.length-1];
-  this.length --;
-  return result;
+  if (result === null) {
+    return {};
+  }
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -8,30 +8,53 @@ var Stack = function() {
 
 };
 
-
-Stack.prototype = {
-
-  size: function() {
+Stack.prototype.size = function() {
     if (this.length <= 0) {
       this.length = 0;
       return this.length;
     }
     return this.length;
-  },
-  push: function(val) {
+};
+
+Stack.prototype.push = function(val) {
     this.storage[this.length] = val;
     this.length ++;
-  },
-  pop: function() {
+};
+
+Stack.prototype.pop = function() {
     var result = this.storage[this.length-1];
     this.length --;
     return result;
-  }
+};
+
+
+
+
+
+// Stack.prototype = {
+
+//   size: function() {
+//     if (this.length <= 0) {
+//       this.length = 0;
+//       return this.length;
+//     }
+//     return this.length;
+//   },
+//   push: function(val) {
+//     this.storage[this.length] = val;
+//     this.length ++;
+//   },
+//   pop: function() {
+//     var result = this.storage[this.length-1];
+//     this.length --;
+//     return result;
+//   }
+//   }
   // ,
   // constructor: function() {
   //   return this.__proto__;
   // }
 
-}
+
 
 
